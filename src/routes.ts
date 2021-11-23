cd8576a2fe0a34609ba47892c996794659f1c983
shaundeveloper <mastercanguro@hotmail.com>
2021-11-23 00:42:40 -0300

    fix: for now the feature to create posts is turned off

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 
 import AuthenticateUserController from './controllers/AuthenticateUserController';
 import ChangeUserInformationController from './controllers/ChangeUserInformationController';
-import CreatePostController from './controllers/CreatePostController';
 import CreateUserController from './controllers/CreateUserController';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
@@ -12,5 +11,4 @@ export const router = Router();
 router
   .post('/signin', AuthenticateUserController.handle)
   .post('/signup', CreateUserController.handle)
-  .post('/create_post', ensureAuthenticated, CreatePostController.handle)
   .post('/settings/account', ensureAuthenticated, ChangeUserInformationController.handle);
